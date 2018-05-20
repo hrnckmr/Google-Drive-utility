449286dcad266875d927be410b9933f81c3fb3f2
hrnckmr <31351944+hrnckmr@users.noreply.github.com>
2018-05-20 10:57:44 +0900

    Update get_driveid.py

--- get_driveid.py
+++ get_driveid.py
@@ -16,8 +16,9 @@ print("***SEARCH FOR*** ", argument[1])
 #service
 import gutil
 service = gutil.service()
-print("name="+argument[1])
-id1=gutil.gquery(service,"name="+"'"+argument[1]+"'")[0]
+query_name="name="+"'"+argument[1]+"'"
+print(quey_name)
+id1=gutil.gquery(service,query_name)[0]
 print("./"+argument[2],id1[2])
 
 cmd="./"+argument[2],id1[2]
