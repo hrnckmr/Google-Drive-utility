7641a37668373fa06e846dd0df3a928acef1ef5c
hrnckmr <31351944+hrnckmr@users.noreply.github.com>
2018-05-20 10:27:08 +0900

    Add files via upload

---
+++ get_driveid.py
@@ -0,0 +1,12 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#UNDER CONSTRUCTION!
+
+import sys
+argument = sys.argv
+for i in argument:
+    print("received argument", i)
+
+print(argument[0])
+
