d0a3be4a054b9ed120dd7fda3b74aff206aecdc1
hrnckmr <31351944+hrnckmr@users.noreply.github.com>
2018-05-20 11:11:26 +0900

    Update get_driveid.py

--- get_driveid.py
+++ get_driveid.py
@@ -20,12 +20,10 @@ print("***SEARCH FOR*** ", argument[1])
 import gutil
 service = gutil.service()
 q_name="name="+"'"+argument[1]+"'"
-print(q_name)
-print("sample:","name='colab_temp'")
 
 id1=gutil.gquery(service,q_name)
-print("query result:",id1)
-print("./"+argument[2],id1[0][2])
+print("***QUERY RESULT***",id1)
+#print("./"+argument[2],id1[0][2])
 
 cmd="./"+argument[2],id1[0][2]
 gutil.gupload(service,cmd[0],cmd[1])
